a014000254f6757283b1c130778e84e15209ca98
kenanbylan <kenan.baylan4654@gmail.com>
2022-08-15 16:43:49 +0300

    frontend edited and esj format adding

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(express.static("public"));
+
+//Routes
 app.get("/", (req, res, next) => {
-  res.status(200).send("Hello World");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+app.get("/about", (req, res, next) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 const port = 3000;
